fdb9e4abec2956bc912c89320f02dbd9ad064f7e
quocduy2003 <duy.tq22942@sinhvien.hoasen.edu.vn>
2025-12-29 15:45:33 +0700

    fix: next.config url image

--- next.config.ts
+++ next.config.ts
@@ -1,43 +1,18 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "http",
-//         hostname: "localhost",
-//         port: "1337",
-//         pathname: "/uploads/**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "**",
-//         port: "",
-//         pathname: "/uploads/**",
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      // 1. Cấu hình cũ cho ảnh demo (Unsplash)
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/**",
-      },
-      // 2. Cấu hình MỚI cho ảnh từ Backend Local (Strapi)
       {
         protocol: "http",
         hostname: "localhost",
-        port: "1337", // Cổng mặc định của Strapi
+        port: "1337",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
         pathname: "/uploads/**",
       },
     ],
@@ -45,3 +20,28 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       // 1. Cấu hình cũ cho ảnh demo (Unsplash)
+//       {
+//         protocol: "https",
+//         hostname: "images.unsplash.com",
+//         port: "",
+//         pathname: "/**",
+//       },
+//       // 2. Cấu hình MỚI cho ảnh từ Backend Local (Strapi)
+//       {
+//         protocol: "http",
+//         hostname: "localhost",
+//         port: "1337", // Cổng mặc định của Strapi
+//         pathname: "/uploads/**",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
